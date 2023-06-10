f609acb1ee1fa93f989e370c3d79365571696a0e
Giulia Real <84285314+RealGiulia@users.noreply.github.com>
2023-06-10 11:27:26 -0300

    example file for testing git

---
+++ aula.py
@@ -0,0 +1,18 @@
+# nome = "Pedrita" 
+# sobrenome = "Real"
+
+# nome_completo = nome + " " + sobrenome
+# print(nome_completo)
+
+# letra = nome[0]
+# print(letra)
+
+lista = ["Giulia","Giulia", 20, 4000.00]
+# lista.pop(1)
+
+# iteravel -> como se fosse algo que pode ser 'percorrido'
+for item in lista:
+    print(item)
+    
+# tupla = ("Adidas", "Nike", "Mizuno")
+# print(tupla)
